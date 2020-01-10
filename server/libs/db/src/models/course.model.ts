7267140add86c51157aa438df9b62408f1a8f8ee
pengwei <1711935344@qq.com>
2020-01-10 10:56:26 +0800

    xiaowei

--- server/libs/db/src/models/course.model.ts
+++ server/libs/db/src/models/course.model.ts
@@ -1,21 +1,27 @@
-import { prop, modelOptions, arrayProp, Ref } from '@typegoose/typegoose'
-import { ApiProperty } from '@nestjs/swagger'
-import { Episode } from './episode.model'
+import { prop, modelOptions, arrayProp, Ref } from '@typegoose/typegoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Episode } from './episode.model';
 
 @modelOptions({
-    schemaOptions: {
-        timestamps: true
-    }
+  schemaOptions: {
+    timestamps: true,
+    toJSON: { virtuals: true }, //定义虚拟字段需要这么写
+  },
 })
 export class Course {
-    @ApiProperty({ description: '课程名称' })
-    @prop()
-    name: string
+  @ApiProperty({ description: '课程名称' })
+  @prop()
+  name: string;
 
-    @ApiProperty({ description: '封面图' })
-    @prop()
-    cover: string
+  @ApiProperty({ description: '封面图' })
+  @prop()
+  cover: string;
 
-    @arrayProp({ itemsRef: 'Episode' })
-    episodes: Ref<Episode>[]
+  //虚拟字段
+  @arrayProp({
+    ref: 'Episode',
+    localField: '_id',
+    foreignField: 'course',
+  })
+  episodes: Ref<Episode>[];
 }
